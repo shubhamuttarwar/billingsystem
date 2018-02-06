1b02d794669b2567c28596d7386e5aa5f9d6d85d
Shubham Uttarwar <shubhamuttarwar14@gmail.com>
2018-02-06 22:44:50 +0530

    added product search

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,6 +1,6 @@
 class ProductsController < ApplicationController
 	def index
-		@product = Product.all
+		@product = Product.search(params[:search])
 
 	end
     
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,5 +5,12 @@ class Product < ApplicationRecord
     validates :sku, presence: true
 
     belongs_to :category
+    def self.search(search)
+    	if search
+    		Product.where("product_name ILIKE ? OR code ILIKE ?", "%#{search}%", "%#{search}%")
+    	else
+    		Product.all.order("updated_at DESC").limit(10)
+    	end
+    end
 
 end
